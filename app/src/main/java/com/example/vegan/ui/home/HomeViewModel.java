2d6b2ec3cb9219c1269ca5348ed72b714578389d
Victor Vu Tran <vuanhtran95@gmail.com>
2023-03-29 20:02:42 +0100

    - Try next commit

--- app/src/main/java/com/example/vegan/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/vegan/ui/home/HomeViewModel.java
@@ -10,7 +10,7 @@ public class HomeViewModel extends ViewModel {
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        mText.setValue("Have a good day");
     }
 
     public LiveData<String> getText() {
